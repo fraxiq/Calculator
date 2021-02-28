3ff321c77442b9abfa83ec2a887c53aba6909767
fraxiq <alexns007@abv.bg>
2021-02-28 16:50:31 +0200

    code

---
+++ Calculator-main/Calc/Calc/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+            string operation = Console.ReadLine();
+            double result = Calculator(num1, num2, operation);
+            Console.WriteLine(result);
+
+        }
+
+        private static double Calculator(double num1, double num2,
+            string operation)
+        {
+            if (operation == "+")
+            {
+                return Add(num1, num2);
+            }
+            else if (operation == "-")
+            {
+                return Subtract(num1, num2);
+            }
+            else if (operation == "*")
+            {
+                return Multiply(num1, num2);
+            }
+            else if (operation == "/")
+            {
+                return Divide(num1, num2);
+            }
+            return 0.0;
+
+        }
+
+        private static double Divide(double num1, double num2)
+        {
+            if (num2 != 0)
+            {
+                return num1 / num2;
+            }
+            else
+            {
+                throw new ArgumentException("You can't divide by 0");
+
+            }
+        }
+
+        private static double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        private static double Subtract(double num1, double num2)
+        {
+            return num1 = num2;
+        }
+
+        private static double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
